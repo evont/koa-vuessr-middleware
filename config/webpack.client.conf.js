5f7ee7b838e1854abb78695512d3682223caa0a8
evont <evontgoh@foxmail.com>
2018-11-10 22:21:53 +0800

    [fix] - webpack optimization option change

--- config/webpack.client.conf.js
+++ config/webpack.client.conf.js
@@ -18,18 +18,10 @@ const config = merge(base, {
     minimize: true,
     noEmitOnErrors: true,
     splitChunks: {
-      chunks: 'async', // 必须三选一： "initial" | "all" | "async"
-      minSize: 30000, // 最小尺寸
-      minChunks: 2, //must be greater than or equal 2. The minimum number of chunks which need to contain a module before it's moved into the commons chunk.
-      maxAsyncRequests: 5, // 最大异步请求数
-      maxInitialRequests: 3, // 最大初始化请求书
-      name: true, // 名称，此选项可接收 function
       cacheGroups: {
         vendor: { // key 为entry中定义的 入口名称
           name: 'vendor', // 要缓存的 分隔出来的 chunk 名称
           chunks: 'all', //all-异步加载快，但初始下载量较大，文件共用性好； initial-初始下载量较小，但异步加载量较大，文件间有重复内容
-          priority: -10,
-          reuseExistingChunk: false, // 选项用于配置在模块完全匹配时重用已有的块，而不是创建新块
           test: /node_modules[\\/]/
         },
       }
